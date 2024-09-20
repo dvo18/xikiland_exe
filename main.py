f94781b4483ce7b873abbe3349006d2478e15d75
Diego Velázquez Ortuño <diegovelazquezortuno.personal@gmail.com>
2024-09-20 03:43:28 +0200

    working test 3

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ def main():
     if not is_admin():
         show_end_alert("Este programa necesita ser ejecutado como administrador")
 
-    #show_end_alert("PRUEBA")
+    show_end_alert("PRUEBA", error=False)
                        
     initializate()
     profiles_management()
